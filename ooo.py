628c64889967bff2d8a4aa864855a9ce7bab200c
stanDee <schanjr@syr.edu>
2015-02-24 22:51:16 -0800

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -68,5 +68,17 @@ class Provider(Super):
 x=Provider()
 print x.delegate()
 '''
+
+class Shape(object):
+    def __init__(self, shapename, **kwds):
+        self.shapename = shapename
+        super(Shape, self).__init__(**kwds)
+
+class ColoredShape(Shape):
+    def __init__(self, color, **kwds):
+        self.color = color
+        super(ColoredShape, self).__init__(**kwds)
+
+cs = ColoredShape(color='red', shapename='circle')
         
 
