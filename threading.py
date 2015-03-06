e4856b14cff952b0ed85f04fca053c39628f2411
stanDee <schanjr@syr.edu>
2015-03-05 16:18:55 -0800

    Create threading.py

---
+++ threading.py
@@ -0,0 +1,77 @@
+import logging
+import threading
+import time
+
+
+#credit to http://pymotw.com/2/threading/
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s',
+                    )
+                    
+def wait_for_event(e):
+    """Wait for the event to be set before doing anything"""
+    logging.debug('wait_for_event starting')
+    event_is_set = e.wait()
+    logging.debug('event set: %s', event_is_set)
+
+def wait_for_event_timeout(e, t):
+    """Wait t seconds and then timeout"""
+    while not e.isSet():
+        logging.debug('wait_for_event_timeout starting')
+        event_is_set = e.wait(t)
+        logging.debug('event set: %s', event_is_set)
+        if event_is_set:
+            logging.debug('processing event')
+        else:
+            logging.debug('doing other work')
+
+
+e = threading.Event()
+t1 = threading.Thread(name='block', 
+                      target=wait_for_event,
+                      args=(e,))
+t1.start()
+
+t2 = threading.Thread(name='non-block', 
+                      target=wait_for_event_timeout, 
+                      args=(e, 2))
+t2.start()
+
+logging.debug('Waiting before calling Event.set()')
+time.sleep(3)
+e.set()
+logging.debug('Event is set')
+
+
+############################################################################################################################
+import threading
+import time
+import logging
+import random
+
+logging.basicConfig(level=logging.DEBUG,format='[%(levelname)s] (%(threadName)-10s) %(message)s',) #Try to comment this out and see what happens
+
+
+
+def worker():
+    """thread worker function"""
+    t = threading.currentThread()
+    pause = random.randint(1,5)
+    logging.debug('sleeping %s', pause)
+    time.sleep(pause)
+    logging.debug('ending')
+    return
+
+for i in range(3):
+    t = threading.Thread(target=worker)
+    t.setDaemon(True)
+    t.start()
+
+main_thread = threading.currentThread()
+for t in threading.enumerate():
+    if t is main_thread:
+        continue
+    logging.debug('joining %s', t.getName())
+    t.join()
